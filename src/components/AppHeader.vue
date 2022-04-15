e757fd1339295f4a305886461c02ed513429a0c6
vikhang2t1 <vikhangngo953@gmail.com>
2022-04-15 23:56:34 +0700

    Update bag sidebar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -60,6 +60,34 @@
             </ul>
         </div>
     </div>
+    <div @click="handleModalClick" class="bag_sidebar" ref="bag_sidebar">
+        <div class="bag_content">
+            <div class="bag_heading clearfix">
+                <h4>Giỏ hàng</h4>
+                <div class="bag_btn_close d-flex justify-content-center" title="Đóng giỏ hàng">
+                    <button class="close_menu" aria-label="Đóng menu" title="Đóng menu">
+                        <i class="fa-solid fa-xmark"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="bag_body">
+
+            </div>
+            <div class="bag_footer">
+                <div class="clearfix">
+                    <div class="d-flex justify-content-between px-3 py-3">
+                        <span class="text-left">Tổng tiền:</span>
+                        <div class="total_price text-right">
+                            <span>350.000đ</span>
+                        </div>
+                    </div>
+                    <div class="bag_btn_checkout">
+                        <button class="btn" title="Thanh toán">Thanh toán</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -71,14 +99,33 @@ export default {
     },
     methods: {
             handleModalClick(e){
-                console.log(e.path[0])
+                // console.log(e.path[0])
                 if(e.path[0].className == 'fa-solid fa-bars'){
                     this.$refs.main_menu.classList.add('active');
                     this.$refs.modal_theme.classList.add('active');
+                    console.log(this.$refs.main_menu.classList);
+                    console.log(this.$refs.modal_theme.classList);
                 }
-                if (e.path[0].className == "fa-solid fa-xmark" || e.path[0].className == "close-menu" || e.path[0].className=="modal_theme active") {
-                    this.$refs.main_menu.classList.remove('active');
+                if(e.path[0].className == 'fa-solid fa-basket-shopping'){
+                    this.$refs.bag_sidebar.classList.add('active-modal');
+                    this.$refs.modal_theme.classList.add('active-modal');
+                }
+                if (e.path[0].className == "fa-solid fa-xmark" || e.path[0].className == "close-menu" || e.path[0].className=="modal_theme active" || e.path[0].className=="modal_theme active-modal" ) {
+                    console.log(e.path[0].className)
+                    if (this.$refs.main_menu.className=='main_menu active'){
+                        this.$refs.main_menu.classList.remove('active');
+                        this.$refs.modal_theme.classList.remove('active');
+                    }
+                    else{
+                        this.$refs.bag_sidebar.classList.remove('active-modal');
+                        this.$refs.modal_theme.classList.remove('active-modal');
+                    }
+                    
+                }
+                if (e.path[0].className=="fa-solid fa-basket-shopping" || e.path[0].className == "close-menu" || e.path[0].className=="modal_theme active"){
+                    this.$refs.bag_sidebar.classList.remove('active');
                     this.$refs.modal_theme.classList.remove('active')
+          
                 }
                 if (e.path[0].textContent ==='Shop' && this.turnOn =='false'){
                     this.turnOn='true'
@@ -116,7 +163,7 @@ export default {
     opacity: 0;
     transition: opacity 0.3s ease-in-out, visibility 0.3s ease-in-out;
 }
-.main_menu{
+.main_menu,.bag_sidebar{
     height: 100vh;
     overflow-y: auto;
     width: 220px;
@@ -131,6 +178,24 @@ export default {
     box-shadow: none;
     touch-action: manipulation;
 }
+.bag_sidebar{
+    right:0;
+    left:auto;
+    width:300px;
+    opacity: 0;
+    transition: opacity 0.5s ease 0.25s, transform 0.5s ease 0.25s;
+    transform: translateX(80px);
+    height: 100%;
+
+}
+.bag_heading{
+    padding:15px 15px;
+}
+.bag_heading h4{
+    font-size:16px;
+    text-transform: uppercase;
+    font-weight: 600;
+}
 .main_menu .content_menu{
     transition: opacity 0.5s ease 0.25s, transform 0.5s ease 0.25s;
     height: 100%;
@@ -138,7 +203,7 @@ export default {
     padding: 10px 0;
     position: relative;
 }
-.content_menu button{
+.content_menu button,.bag_heading button{
     position: absolute;
     margin-left: 0;
     right: 10px;
@@ -153,11 +218,35 @@ export default {
     z-index: 999;
     transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
 }
-.content_menu button i{
+.bag_heading button{
+    top:15px;
+    font-size:1.4rem;
+    right:14px;
+    font-weight: 600;
+}
+.bag_body{
+    height:calc(100vh - 250px);
+    padding:15px;
+}
+.bag_footer{
+    border-top:1px dashed #ccc;
+}
+.bag_btn_checkout{
+    padding:20px 10px;
+}
+.bag_btn_checkout .btn{
+    display:block;
+    width:100%;
+    background-color: aquamarine;
+    color:white;
+}
+.content_menu button i
+{
     font-size: 1.2rem;
     color: #897e7e;
 }
-.content_menu button:hover{
+.content_menu button:hover,
+.bag_heading button:hover{
     transform: translate(0%, 0%) rotate(360deg);
 }
 .nav_list {
@@ -194,13 +283,18 @@ export default {
     display: block;
     padding-left:25px;
 }
-.modal_theme.active{
+.modal_theme.active,
+.modal_theme.active-modal{
     visibility: visible;
     opacity: 0.5;
 }
-.main_menu.active {
+.main_menu.active,
+.bag_sidebar.active-modal {
     visibility: visible;
     transform: translateX(0);
+}
+.bag_sidebar.active-modal{
+    opacity: 1;
 }
 .subnav_list.block_scope{
     display:block;
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,140 +1,176 @@
 <template>
-    <div id='register'>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12 col-lg wrapbox_heading">
-                    <div class="header_page">
-                        <h3>Tạo tài khoản</h3>
-                    </div>
-                </div>
-                <div class="col-md-12 col-lg wrapbox_content">
-                    <div class="form_account">
-                        <form @submit.prevent="CreateAccount" method="POST" accept-charset="UTF-8" :validation-schema="registerFormSchema">
-                            <div class="name clearfix large_form">
-                                <input name="name" type="text" placeholder="Họ tên" id="name" v-model='user.name'>
-                                <ErrorMessage name="name" class="error-feedback" />
-                            </div>
-                            <div class="email clearfix large_form">
-                                <input name="email" type="email" placeholder="Email" id="email" v-model="user.email">
-                                <ErrorMessage name="email" class="error-feedback" />
-                            </div>
-                            <div class="password clearfix large_form">
-                                <input name="password" type="password" placeholder="Mật khẩu" id="password" v-model='user.password'>
-                                <ErrorMessage name="password" class="error-feedback" />
-                            </div>
-                            <div class="phone clearfix large_form">
-                                <input name="phone" type="text" placeholder="Điện thoại" id="phone" v-model='user.phone'>
-                                <ErrorMessage name="phone" class="error-feedback" />
-                            </div>
-                            <p>{{message}}</p>
-                            <div class="register_btn clearfix dark">
-                                <button class="btn">Đăng ký</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  <div id="register">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12 col-lg wrapbox_heading">
+          <div class="header_page">
+            <h3>Tạo tài khoản</h3>
+          </div>
         </div>
+        <div class="col-md-12 col-lg wrapbox_content">
+          <Form
+            @submit="CreateAccount"
+            :validation-schema="registerFormSchema"
+          >
+            <div class="form-group">
+              <label for="name">Tên</label>
+              <Field
+                name="name"
+                type="text"
+                class="form-control "
+                v-model="registerUser.name"
+              />
+              <ErrorMessage name="name" class="error-feedback" />
+            </div>
+            <div class="form-group">
+              <label for="email">E-mail</label>
+              <Field
+                name="email"
+                type="email"
+                class="form-control"
+                v-model="registerUser.email"
+              />
+              <ErrorMessage name="email" class="error-feedback" />
+            </div>
+            <div class="form-group">
+              <label for="password">Mật khẩu</label>
+              <Field
+                name="password"
+                type="password"
+                class="form-control"
+                v-model="registerUser.password"
+              />
+              <ErrorMessage name="password" class="error-feedback" />
+            </div>
+            <div class="form-group">
+              <label for="phone">Điện thoại</label>
+              <Field
+                name="phone"
+                type="tel"
+                class="form-control"
+                v-model="registerUser.phone"
+              />
+              <ErrorMessage name="phone" class="error-feedback" />
+            </div>
+            <div class="form-group">
+              <button class="btn btn-primary">Lưu</button>
+            </div>
+          </Form>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import ContactService from '../services/contact.service';
+import ContactService from "../services/contact.service";
 import * as yup from "yup";
-import {  ErrorMessage } from "vee-validate";
+import { Form, Field, ErrorMessage } from "vee-validate";
 export default {
-    name:'Register',
-    data(){
-        const registerFormSchema = yup.object().shape({
-            name: yup
-                .string()
-                .required("Tên phải có giá trị.")
-                .min(2, "Tên phải ít nhất 3 ký tự.")
-                .max(50, "Tên có nhiều nhất 50 ký tự."),
-            email: yup
-                .string()
-                .email("E-mail không đúng.")
-                .max(50, "E-mail tối đa 50 ký tự.")
-                .required("Email không được bỏ trống"),
-            password: yup
-                .string()
-                .max(32, "Mật khẩu tối đa 32 ký tự.")
-                .min(8,"Mật khẩu tối thiểu phải có 8 ký tự")
-                .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-                    "Mật khẩu nên chứa tối thiểu 8 ký tự, có ít nhất 1 ký tự in hoa,1 ký tự thường và 1 chữ số"
-            ),
-            phone: yup
-                .string()
-                .matches(
-                    /((09|03|07|08|05)+([0-9]{8})\b)/g,
-                    "Số điện thoại không hợp lệ."
-                ),
-        });
-        return{
-            user:{
-                name:'',
-                password:'',
-                phone:'',
-                email:'',
-            },
-            message:"",
-        }
+  name: "Register",
+  data() {
+    const registerFormSchema = yup.object().shape({
+      name: yup
+        .string()
+        .required("Tên phải có giá trị.")
+        .min(2, "Tên phải ít nhất 3 ký tự.")
+        .max(50, "Tên có nhiều nhất 50 ký tự."),
+      email: yup
+        .string()
+        .email("E-mail không đúng.")
+        .max(50, "E-mail tối đa 50 ký tự.")
+        .required("Email không được bỏ trống"),
+      password: yup
+        .string()
+        .max(32, "Mật khẩu tối đa 32 ký tự.")
+        .min(8, "Mật khẩu tối thiểu phải có 8 ký tự")
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+          "Mật khẩu nên chứa tối thiểu 8 ký tự, có ít nhất 1 ký tự in hoa,1 ký tự thường và 1 chữ số"
+        ),
+      phone: yup
+        .string()
+        .matches(
+          /((09|03|07|08|05)+([0-9]{8})\b)/g,
+          "Số điện thoại không hợp lệ."
+        ),
+    });
+    return {
+      registerUser: {
+        name: "",
+        password: "",
+        phone: "",
+        email: "",
+      },
+      message: "",
+      registerFormSchema,
+    };
+  },
+  props: {
+        user: { type: Object, required: true }
     },
-    methods:{
-        async CreateAccount() {
-                try{
-                    await ContactService.create(this.user);
-                    this.message = "Tài khoản đã được tạo thành công.";
-                }
-                catch(data){
-                    console.log(data);
-                }
-            },
+  methods: {
+    async CreateAccount() {
+      try {
+            await ContactService.create(this.registerUser)
+            .then((data)=>{
+                console.log(data);
+            })
+            .catch((data)=>{
+                console.log(data);
+            })
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
     },
-    components: {
-        ErrorMessage,
-    },
-}
+  },
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+};
 </script>
 <style scoped>
-.wrapbox_heading{
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    height:80vh;
-    border-right:1px solid #ccc;
+.wrapbox_heading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+  border-right: 1px solid #ccc;
 }
-.header_page h3{
-    font-size: 40px;
-    font-weight: 700;
+.header_page h3 {
+  font-size: 40px;
+  font-weight: 700;
 }
-.header_page:after{
-    content: "";
-    background: #000;
-    display: block;
-    width: 60px;
-    height: 4px;
-    margin-top: 70px;
+.header_page:after {
+  content: "";
+  background: #000;
+  display: block;
+  width: 60px;
+  height: 4px;
+  margin-top: 70px;
 }
-.wrapbox_content{
-    padding: 120px 60px;
+.wrapbox_content {
+  padding: 120px 60px;
 }
-.large_form{
-    margin-bottom: 30px;
+.large_form {
+  margin-bottom: 30px;
 }
-.form_account input[type='text'],
-.form_account input[type='email'],
-.form_account input[type='password']{
-    width:100%;
-    padding:0 20px;
-    height: 55px;
-    border: 1px solid transparent;
-    background: #ccc;
-    color:#000
+.form_account input[type="text"],
+.form_account input[type="email"],
+.form_account input[type="password"] {
+  width: 100%;
+  padding: 0 20px;
+  height: 55px;
+  border: 1px solid transparent;
+  background: #ccc;
+  color: #000;
 }
-.btn{
-    background-color: black;
-    color:white;
-    padding:15px 30px;
-    border:1px solid transparent;
+.btn {
+  background-color: black;
+  color: white;
+  padding: 15px 30px;
+  border: 1px solid transparent;
 }
 </style>
